0e28ea1d5d2d6458d322780a6c4bbac424f62f06
xuwei-k <6b656e6a69@gmail.com>
2018-11-08 09:33:52 +0900

    filter scalaz-stream

--- modules/core/src/main/scala/eu/timepit/scalasteward/update/FilterAlg.scala
+++ modules/core/src/main/scala/eu/timepit/scalasteward/update/FilterAlg.scala
@@ -54,6 +54,8 @@ object FilterAlg {
 
           case ("org.scala-sbt", "sbt-launch", _) => false
 
+          case ("org.scalaz.stream", "scalaz-stream", "0.8.6") => false
+
           case ("org.scalaz", _, ScalazVersions()) => false
 
           case ("javax.servlet", "javax.servlet-api", _) => false
